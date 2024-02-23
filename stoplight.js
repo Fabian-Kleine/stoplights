91468806ea439b2a95621b6ea966c121b1cfd186
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-02-23 14:39:15 +0100

    added pedestrian stoplights
    added pedestrian stoplights that can be requested by a button press

--- stoplight.js
+++ stoplight.js
@@ -9,6 +9,11 @@ const stoplightRightDOM = document.getElementById("stoplightRight");
 const stoplightLeftDOM = document.getElementById("stoplightLeft");
 const stoplightBottomDOM = document.getElementById("stoplightBottom");
 
+const pedestrianStoplightTopDOM = document.getElementById("pedestrianStoplightTop");
+const pedestrianStoplightRightDOM = document.getElementById("pedestrianStoplightRight");
+const pedestrianStoplightLeftDOM = document.getElementById("pedestrianStoplightLeft");
+const pedestrianStoplightBottomDOM = document.getElementById("pedestrianStoplightBottom");
+
 class Stoplight {
     constructor(stoplight) {
         this.DOMObject = stoplight;
@@ -23,22 +28,41 @@ class Stoplight {
 
     async setRed() {
         this.greenLight.classList.remove("active");
-        this.yellowLight.classList.add("active");
+        if (this.yellowLight) this.yellowLight.classList.add("active");
         this.activeLight = "yellow";
-        await delay(1000)
-        this.yellowLight.classList.remove("active");
+        if (this.yellowLight) await delay(1500)
+        if (this.yellowLight) this.yellowLight.classList.remove("active");
         this.redLight.classList.add("active");
         this.activeLight = "red"
     }
 
     async setGreen() {
-        this.yellowLight.classList.add("active");
+        if (this.yellowLight) this.yellowLight.classList.add("active");
         this.activeLight = "yellow";
-        await delay(1000)
+        await delay(1500)
         this.redLight.classList.remove("active");
-        this.yellowLight.classList.remove("active");
+        if (this.yellowLight) this.yellowLight.classList.remove("active");
         this.greenLight.classList.add("active");
         this.activeLight = "green"
+    }
+
+    requestSignal() {
+        this.pedestrianButton.classList.add("active");
+    }
+
+    async requestGreen() {
+        const active = this.pedestrianButton.classList.contains("active");
+        if (active) {
+            this.pedestrianButton.classList.remove("active");
+            await this.setGreen();
+        }
+    }
+
+    bindButton(button) {
+        this.pedestrianButton = button;
+        button.addEventListener("click", () => {
+            this.requestSignal()
+        })
     }
 }
 
@@ -47,20 +71,47 @@ const stoplightRight = new Stoplight(stoplightRightDOM);
 const stoplightLeft = new Stoplight(stoplightLeftDOM);
 const stoplightBottom = new Stoplight(stoplightBottomDOM);
 
-async function runStoplights() {
+const pedestrianStoplightTop = new Stoplight(pedestrianStoplightTopDOM);
+const pedestrianStoplightRight = new Stoplight(pedestrianStoplightRightDOM);
+const pedestrianStoplightLeft = new Stoplight(pedestrianStoplightLeftDOM);
+const pedestrianStoplightBottom = new Stoplight(pedestrianStoplightBottomDOM);
+
+//pedestrian buttons
+const pedestrianStoplightBottomButton = document.getElementById('pedestrianStoplightBottomButton');
+const pedestrianStoplightTopButton = document.getElementById("pedestrianStoplightTopButton");
+const pedestrianStoplightLeftButton = document.getElementById("pedestrianStoplightLeftButton");
+const pedestrianStoplightRightButton = document.getElementById("pedestrianStoplightRightButton");
+
+pedestrianStoplightTop.bindButton(pedestrianStoplightRightButton);
+pedestrianStoplightBottom.bindButton(pedestrianStoplightLeftButton);
+pedestrianStoplightRight.bindButton(pedestrianStoplightBottomButton);
+pedestrianStoplightLeft.bindButton(pedestrianStoplightTopButton);
+
+
+async function stoplightsCycle() {
     stoplightTop.setRed();
     stoplightBottom.setRed();
+    pedestrianStoplightTop.setRed();
+    pedestrianStoplightBottom.setRed();
     await delay(3000);
     stoplightLeft.setGreen();
     stoplightRight.setGreen();
+    pedestrianStoplightLeft.requestGreen();
+    pedestrianStoplightRight.requestGreen();
+
     await delay(10000);
+
     stoplightLeft.setRed();
     stoplightRight.setRed();
+    pedestrianStoplightLeft.setRed();
+    pedestrianStoplightRight.setRed();
     await delay(3000);
     stoplightTop.setGreen();
     stoplightBottom.setGreen();
+    pedestrianStoplightTop.requestGreen();
+    pedestrianStoplightBottom.requestGreen();
 }
 
-runStoplights()
+stoplightsCycle()
 
-setInterval(() => runStoplights(), 20000);
+setInterval(() => stoplightsCycle(), 30000);
